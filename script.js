567bbc65f7d757988c384a5b43aba29e40b2409c
Carlos Eduardo <eduardo-1942@hotmail.com>
2022-09-27 20:07:43 -0300

    atividades pós feedback

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 const frutas = ["laranja", "limão", "uva"];
+//exercicio 1
+const listaFrutas = document.getElementById("lista-de-frutas")
+const fruta1 = document.getElementById("fruta-1")
+const fruta2 = document.getElementById("fruta-2")
+const fruta3 = document.getElementById("fruta-3")
+const fruta4 = document.getElementById("fruta-4")
+
+// for (let i = 0; i < frutas.length; i++) {
+//   document.getElementById(`fruta-${[i + 1]}`).innerHTML = frutas[i]
+// }
+
+//
+fruta1.innerHTML = frutas[0];
+fruta2.innerHTML = frutas[1];
+fruta3.innerHTML = frutas[2];
+
+const adicionarFruta = document.getElementById('fruta');
+let novaFruta = () => {
+    fruta4.innerHTML = adicionarFruta.value
+}
+
+function input() {
+    console.log(fruta.value)
+}
